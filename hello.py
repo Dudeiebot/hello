212ab6f329fea18e41ba224638a3ebb1dda8bda7
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-06 18:06:39 +0100

    learn_first

--- hello.py
+++ hello.py
@@ -88,10 +88,10 @@ elif b == a:
 else:
     print("the other report")
 
-i = 1
-while i < 6:
-    print(i)
-    if i == 3:
+a = 1
+while a < 6:
+    print(a)
+    if a == 3:
         break
-    i += 1
+    a += 1
  
