22200883f160b56110a7d3aadf73047e4c10bf40
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-07 18:17:49 +0100

    added note

--- hello.py
+++ hello.py
@@ -126,3 +126,12 @@ function_main("siddiq")
 function_main("penny")
 function_main("tally")
 function_main("yeats")
+
+#these practically add up all the factorial value of n up but you can check up the one that lsit them all in fn.py
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print("The factorial of 5 is", factorial(5))
