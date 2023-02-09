2276799bb0fe7a3c66eb4a14a33b191f1ecd923f
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-09 18:57:05 +0100

    pushin import

--- import.py
+++ import.py
@@ -1,5 +1,10 @@
-import fn
+import fn      #import from fn.py and when you run it import all the output of fn.py and also the last line of fn.py(ln 57 & 58)
 fn.greetings("morning")
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+print(x.strftime("%H"))  #strftime() inputs some certain format code in. just like these
 
 
  
