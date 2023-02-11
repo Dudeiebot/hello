4aab73cf1e88abf2dba0ada795e1312991aa9f54
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-11 18:51:20 +0100

    calling files

--- F.py
+++ F.py
@@ -3,4 +3,6 @@ for x in f:
     print(x)
 
 f = open("/Users/Dudeiebeech/hello-1/bullish.txt", "r")
-print(f.read(5))
+print(f.read(5))   
+
+#kind of having issues here, have to debug before it run well, maybe i got to find out what is happening later on the day.
