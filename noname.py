08c24dca13603f332cb2663130d922d6f87a9bb0
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-15 15:27:05 +0100

    adding minimal color

--- noname.py
+++ noname.py
@@ -5,10 +5,11 @@ matplotlib.use('QtAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 150])
+xpoints = np.array([0, 6, 80, 10])
+ypoints = np.array([20, 50, 10, 40])
 
-plt.plot(xpoints, ypoints)
+plt.plot (xpoints)  
+plt.plot(ypoints)   
 plt.show()
 
 plt.savefig(sys.stdout.buffer)
