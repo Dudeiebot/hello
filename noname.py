7d32226f3cf4a1835f02b3f8c99d401549d0b1f2
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-15 16:42:48 +0100

    making it more cool

--- noname.py
+++ noname.py
@@ -8,8 +8,17 @@ import numpy as np
 xpoints = np.array([0, 6, 80, 10])
 ypoints = np.array([20, 50, 10, 40])
 
-plt.plot (xpoints)  
-plt.plot(ypoints)   
+plt.subplot(1, 2, 1)
+plt.plot(xpoints)  
+plt.plot(ypoints) 
+
+x = np.array([3, 9, 6, 12])
+y = np.array([20, 60, 40, 100])
+
+plt.subplot(1, 2, 2)  #talking about rows and columns that means these have 1 row and 2 columns
+plt.plot(x)
+plt.plot(y)
+
 plt.show()
 
 plt.savefig(sys.stdout.buffer)
