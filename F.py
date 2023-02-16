2136a52d31534490dfc583545f88b85eb0273a4e
Dudecell <77306474+Dudecell@users.noreply.github.com>
2023-02-16 17:29:37 +0100

    Reading .csv file

--- F.py
+++ F.py
@@ -1,10 +1,15 @@
-f = open("/Users/Dudeiebeech/hello-1/bullish.txt", "r")
-for x in f:
-    print(x)
+#f = open("/Users/Dudeiebeech/hello-1/bullish.txt", "r")
+#for x in f:
+ #   print(x)
 
-f = open("/Users/Dudeiebeech/hello-1/bullish.txt", "r")
-print(f.read(5))   
+#f = open("/Users/Dudeiebeech/hello-1/bullish.txt", "r")
+#print(f.read(5))   
 
-#kind of having issues here, have to debug before it run well, maybe i got to find out what is happening later on the day.
+#kind of having issues here, have to debug before it run well, maybe i got to find out what is happening later on the day, for posterity it is in connection with opening a text file on python
 
-f = ("open.txt", "w")
+#f = ("open.txt", "w")
+
+import pandas
+df = pandas.read_csv("/Users/Dudeiebeech/hello-1/data.csv")
+
+print(df)
