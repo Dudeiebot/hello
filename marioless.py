1020296ae746e15d0542b090cdd51362a66e718f
Dudecell <ojoexcel5@gmail.com>
2023-02-20 00:59:05 +0100

    indentation issue

---
+++ marioless.py
@@ -0,0 +1,10 @@
+
+while True:
+    height = int(input("Tell me the Height: "))
+    if height >= 0 and height <= 15:
+        break
+    else:
+        print("please a number between 0 and 15")
+
+for i in range(height):
+    print(' ' * (height - i - 1) + '#' * (i + 1))
