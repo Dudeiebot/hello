e223aac0e5ff41eba2ef131e005cc171ce69658b
Dudecell <ojoexcel5@gmail.com>
2023-02-20 01:46:15 +0100

    mario more

--- marioless.py
+++ marioless.py
@@ -7,4 +7,4 @@ while True:
         print("please a number between 0 and 15")
 
 for i in range(height):
-    print(' ' * (height - i - 1) + '#' * (i + 1))
+    print(' ' * (height - i - 1) + '#' * (i + 1) + '  ' + '#' * (i + 1)) #added some other shiit for mario more
