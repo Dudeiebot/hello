7bd049f3d961793b9251436b28635893bbe48a10
Dudecell <ojoexcel5@gmail.com>
2023-02-21 23:48:31 +0100

    creditcard and all

---
+++ Readme for creditcard.txt
@@ -0,0 +1,48 @@
+
+
+Luhn Algorithm for Credit Card Validation   "make these big when you change it to readme.md file"
+
+This is a Python implementation of the Luhn Algorithm, a simple checksum formula used to validate credit card numbers.
+
+Requirements
+
+To run this code, you will need:
+Python 3.x
+
+Usage
+
+To use the Luhn Algorithm, simply call the get_credit_card_number() function and pass in the credit card number as a string:
+
+'''
+def get_credit_card_number():
+    while True:
+        input_str = input("Enter the credit card number: ")  #
+        if not input_str.isdigit():
+            print("Invalid input. Please enter a positive integer.")               
+            continue
+        user_input = int(input_str)
+        if user_input <= 0:
+            print("Invalid input. Please enter a positive integer.")
+            continue
+        return user_input
+'''
+
+How it works
+
+The Luhn Algorithm works by summing up the digits of the credit card number, starting from the rightmost digit and working backwards. For every other digit, starting with the second-to-last digit, the digit is doubled and the sum of the digits of the resulting number is added to the total sum. Finally, the total sum is divided by 10, and the credit card number is valid if the remainder is 0.
+
+For example, consider the credit card number 4111 1111 1111 1111. Starting from the rightmost digit (which is 1), we add it to the total sum (which is currently 0). Moving to the next digit (which is 1 again), we double it to get 2, and then add the digits of the resulting number (2) to the total sum (which is now 2). Continuing in this manner, we eventually get a total sum of 16. Since 16 is not divisible by 10, the credit card number is not valid.
+
+Testing
+
+To run the unit tests, simply run the following command:
+'python creditcard.py'
+This will run a series of tests to verify that the get_credit_card_number() function correctly identifies valid and invalid credit card numbers.
+
+Contributing
+
+If you find a bug or have a suggestion for how to improve this code, please feel free to open an issue or submit a pull request.
+
+License
+
+This code is licensed under the MIT license. See the LICENSE file for details.
--- creditcard.py
+++ creditcard.py
@@ -1,8 +1,15 @@
+'''
+So here we practically trying to figure out a fraud credit card or maybe a program used to check  your credit card at an automatic teller machine 
+These particular algorithm uses the algorithm invented by Hans Peter Luhn of IBM and have proof to be efficient.
+* so for the beginning if your credit card does not have 2fa then i will tell you to be careful in inputtiny your credit card number on the program but another thing you should be aware of is that i am not storing your credit card number in any DB and these is open source also.
+* so i will not be able to know your credit card number when inputted or being checked for validation on the code and as a developer you can tweak anything you want on these but these is not for everyone 
+also as time goes on i will probably to get a frontend on it but fot now just the algorithm is available.
+'''
 def get_credit_card_number():
     while True:
-        input_str = input("Enter the credit card number: ")
+        input_str = input("Enter the credit card number: ")  #
         if not input_str.isdigit():
-            print("Invalid input. Please enter a positive integer.")                #come back here to add comments
+            print("Invalid input. Please enter a positive integer.")               
             continue
         user_input = int(input_str)
         if user_input <= 0:
@@ -10,7 +17,7 @@ def get_credit_card_number():
             continue
         return user_input
 
-def calculate_luhn_checksum(card_number):
+def calculate_luhn(card_number):      #calaculate_luhn here is used for calculating the luhn algorithm, and you can make more research regarding the algorithm, the algorithm haves a lot of mathematics attached to it also with selecting of the credit card number digit individually and performing a certain operation on it and returning a certain results.
     digit_count = 0
     luhn_sum = 0
     while card_number > 0:
@@ -22,11 +29,12 @@ def calculate_luhn_checksum(card_number):
             product = digit * 2
             luhn_sum += (product // 10) + (product % 10)
         else:
-            luhn_sum += digit
+            luhn_sum += digit   
        
     return luhn_sum
 
-def get_card_issuer(card_number):
+def get_card_issuer(card_number):       #so i am practically not ending it here but i am going to try to figure out the issuer of the credit card also, it can be more easy or more complicated depending on if our card issuer amend their defaults starting number or second number defaults
+                                        #but you can also make your own tweak any new changes you want on the code and see how it works.
     first_digit = card_number
     while first_digit >= 10:
         first_digit //= 10
@@ -35,7 +43,7 @@ def get_card_issuer(card_number):
     while first_two_digits >= 100:
         first_two_digits //= 10
     
-    second_digit = calculate_luhn_checksum(card_number)
+    second_digit = calculate_luhn(card_number)
     if len(str(second_digit)) > 1 and str(second_digit)[1] == "0":
         print("The card is valid", end = " ")
     else:
